592ca596f25d830ea476941738177000e1fa8b39
alenjoo <johnalen931@gmail.com>
2024-12-09 19:42:06 +0530

    db added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'home_screen.dart';
 import 'second_screen.dart';
 import 'third_screen.dart';
