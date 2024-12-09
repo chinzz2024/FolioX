3815c0b64802e5cdc4a3b635b5de3e99d439f2cd
chinzz2024 <pranavmohanindeevaram@gmail.com>
2024-12-09 22:41:29 +0530

    FGH

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
+
 import 'login_page.dart';
-=======
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'home_screen.dart';
-import 'second_screen.dart';
-import 'third_screen.dart';
->>>>>>> 57cd0f84412943186fce6568757c74cebe049414
+
 
 void main() {
   runApp(MyApp());
